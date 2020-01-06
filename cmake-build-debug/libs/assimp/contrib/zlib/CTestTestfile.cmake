2e8ebd6f084a2be1aa16af2ae536bc5275599183
Colby Rodeheaver <colbyrodeheaver@hotmail.com>
2020-01-06 17:17:23 -0600

    removed build folder

--- cmake-build-debug/libs/assimp/contrib/zlib/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/workspace/opengl-boiler/libs/assimp/contrib/zlib
-# Build directory: C:/workspace/opengl-boiler/cmake-build-debug/libs/assimp/contrib/zlib
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
--- cmake-build-debug/libs/glm/glm/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/workspace/opengl-boiler/libs/glm/glm
-# Build directory: C:/workspace/opengl-boiler/cmake-build-debug/libs/glm/glm
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
