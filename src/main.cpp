473f78eda2ff0db017a3bd474b812b0e97902456
Colby Rodeheaver <colbyrodeheaver@hotmail.com>
2020-01-06 23:06:41 -0600

    created two rotating cubes

--- src/main.cpp
+++ src/main.cpp
@@ -29,15 +29,49 @@ int main()
 
 
     float vertices[] = {
-            // positions         // texture coords
-             0.5f,  0.5f, 0.0f,  1.0f, 1.0f, // top right
-             0.5f, -0.5f, 0.0f,  1.0f, 0.0f, // bottom right
-            -0.5f, -0.5f, 0.0f,  0.0f, 0.0f, // bottom left
-            -0.5f,  0.5f, 0.0f,  0.0f, 1.0f  // top left
+            -0.5f,0.5f,-0.5f,   0,0,
+            -0.5f,-0.5f,-0.5f,  0,1,
+            0.5f,-0.5f,-0.5f,   1,1,
+            0.5f,0.5f,-0.5f,    1,0,
+
+            -0.5f,0.5f,0.5f,    0,0,
+            -0.5f,-0.5f,0.5f,   0,1,
+            0.5f,-0.5f,0.5f,    1,1,
+            0.5f,0.5f,0.5f,     1,0,
+
+            0.5f,0.5f,-0.5f,    0,0,
+            0.5f,-0.5f,-0.5f,   0,1,
+            0.5f,-0.5f,0.5f,    1,1,
+            0.5f,0.5f,0.5f,     1,0,
+
+            -0.5f,0.5f,-0.5f,   0,0,
+            -0.5f,-0.5f,-0.5f,  0,1,
+            -0.5f,-0.5f,0.5f,   1,1,
+            -0.5f,0.5f,0.5f,    1,0,
+
+            -0.5f,0.5f,0.5f,    0,0,
+            -0.5f,0.5f,-0.5f,   0,1,
+            0.5f,0.5f,-0.5f,    1,1,
+            0.5f,0.5f,0.5f,     1,0,
+
+            -0.5f,-0.5f,0.5f,   0,0,
+            -0.5f,-0.5f,-0.5f,  0,1,
+            0.5f,-0.5f,-0.5f,   1,1,
+            0.5f,-0.5f,0.5f,    1,0
     };
     unsigned int indices[] = {
-            0, 1, 3, // first triangle
-            1, 2, 3  // second triangle
+            0,1,3,
+            3,1,2,
+            4,5,7,
+            7,5,6,
+            8,9,11,
+            11,9,10,
+            12,13,15,
+            15,13,14,
+            16,17,19,
+            19,17,18,
+            20,21,23,
+            23,21,22
     };
     unsigned int VBO, VAO, EBO;
     glGenVertexArrays(1, &VAO);
@@ -71,21 +105,30 @@ int main()
 
         // render
         // ------
+        glEnable(GL_DEPTH_TEST);
         glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
         // render container
         program.start();
 
-        auto translation = glm::vec3(0.0f, 0.0f, 0.0f);
+        auto translation = glm::vec3(-0.25f, 0.0f, 0.0f);
 
-        auto trans = Maths::createTransformationMatrix(translation, rotation,rotation,0,1);
+        auto trans = Maths::createTransformationMatrix(translation, rotation,rotation,0,.5);
 
         unsigned int transformLoc = program.getUniformLocation("transform");
         glUniformMatrix4fv(transformLoc, 1, GL_FALSE, glm::value_ptr(trans));
 
         glBindVertexArray(VAO);
-        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+        glDrawElements(GL_TRIANGLES, 36, GL_UNSIGNED_INT, 0);
+
+        // These are the steps needed to create a new square
+        translation = glm::vec3(0.25f, 0.0f, 0.0f);
+        trans = Maths::createTransformationMatrix(translation, rotation,rotation,0,.5);
+        glUniformMatrix4fv(transformLoc, 1, GL_FALSE, glm::value_ptr(trans));
+
+        glBindVertexArray(VAO);
+        glDrawElements(GL_TRIANGLES, 36, GL_UNSIGNED_INT, 0);
 
         // glfw: swap buffers and poll IO events (keys pressed/released, mouse moved etc.)
         // -------------------------------------------------------------------------------
